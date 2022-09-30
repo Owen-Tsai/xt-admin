46fdbcdb4b31259120708ba95bfafa33caca7e2a
Owen Tsai <owentsai.v@gmail.com>
2022-09-30 15:30:40 +0800

    sync

---
+++ src/components/form-designer/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">
+    <a-layout class="h-full">
+      <a-layout-sider class="!shadow-none border-r w-[200px]">345</a-layout-sider>
+      <a-layout-content class="bg-gray-50 px-4">
+        <!-- insert widget-form -->
+      </a-layout-content>
+      <a-layout-sider class="!shadow-none border-l w-[200px]">123</a-layout-sider>
+    </a-layout>
+  </div>
+</template>
