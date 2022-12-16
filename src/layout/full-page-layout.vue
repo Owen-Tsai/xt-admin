7b09e271317c826a1e11419503e7f210a77047fa
Owen Tsai <owentsai.v@gmail.com>
2022-12-16 16:08:19 +0800

    fix: use route meta for rendering full-page-layout title

--- src/layout/full-page-layout.vue
+++ src/layout/full-page-layout.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout class="h-full">
-    <tool-bar title="表单设计器" />
+    <tool-bar :title="title" />
     <a-layout-content>
       <page-view />
     </a-layout-content>
@@ -8,6 +8,14 @@
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useRoute } from 'vue-router'
 import ToolBar from '@/components/tool-bar.vue'
 import PageView from './inner-layout.vue'
+
+const { meta } = useRoute()
+const { t } = useI18n()
+
+const title = computed(() => (meta.locale ? t(meta.locale) : meta.text))
 </script>
