cef78e11354fc04fec901ffc2bc66ac1ee5fc888
wuchang <2481893764@qq.com>
2023-01-05 09:51:19 +0800

    简写config，统一widget。

--- src/components/form-designer/config/inputnumber-config.vue
+++ src/components/form-designer/config/inputnumber-config.vue
@@ -1,53 +1,53 @@
 <template>
   <a-form-item label="字段标识">
-    <a-input v-model="config.config.id" allow-clear />
+    <a-input v-model="widget.id" allow-clear />
   </a-form-item>
   <a-form-item label="字段标签">
-    <a-input v-model="config.config.label" allow-clear />
+    <a-input v-model="widget.label" allow-clear />
   </a-form-item>
   <a-form-item label="宽度">
     <a-input
-      v-model="config.config.width"
+      v-model="widget.width"
       placeholder="输入含单位(%/px)的数值"
       allow-clear
     />
   </a-form-item>
   <a-form-item label="默认值">
-    <a-input-number v-model="config.config.defaultValue" allow-clear />
+    <a-input-number v-model="widget.defaultValue" allow-clear />
   </a-form-item>
   <a-form-item label="提示文字">
-    <a-input v-model="config.config.placeholder" />
+    <a-input v-model="widget.placeholder" />
   </a-form-item>
   <a-form-item label="精度">
-    <a-input-number v-model="config.config.precision" />
+    <a-input-number v-model="widget.precision" />
   </a-form-item>
   <a-form-item label="步长">
-    <a-input-number v-model="config.config.step" />
+    <a-input-number v-model="widget.step" />
   </a-form-item>
   <div class="boolean-config mt-4">
     <span class="label">是否禁用</span>
-    <a-switch v-model="config.config.disabled" />
+    <a-switch v-model="widget.disabled" />
   </div>
   <div class="boolean-config mt-4">
     <span class="label">是否必填</span>
-    <a-switch v-model="config.config.required" />
+    <a-switch v-model="widget.required" />
   </div>
   <div class="boolean-config mt-4">
     <span class="label">是否只读</span>
-    <a-switch v-model="config.config.readonly" />
+    <a-switch v-model="widget.readonly" />
   </div>
   <div class="boolean-config mt-4">
     <span class="label">允许清除</span>
-    <a-switch v-model="config.config.allowClear" />
+    <a-switch v-model="widget.allowClear" />
   </div>
   <a-form-item label="自定义校验规则">
     <a-textarea
-      v-model="config.config.rules"
+      v-model="widget.rules"
       :auto-size="{ minRows: 4, maxRows: 6 }"
     />
   </a-form-item>
   <a-form-item label="校验触发时机">
-    <a-select v-model="config.config.trigger" :allow-search="false" multiple>
+    <a-select v-model="widget.trigger" :allow-search="false" multiple>
       <a-option
         v-for="opt in inputEventNames"
         :key="opt"
@@ -70,8 +70,8 @@ const props = defineProps({
     required: true,
   },
 })
-const config = computed({
-  get: () => props.widgetConfig,
+const widget = computed({
+  get: () => props.widgetConfig.config,
   set: (val) => {
     emit('update:widgetConfig', val)
   },
--- src/components/form-designer/config/rate-config.vue
+++ src/components/form-designer/config/rate-config.vue
@@ -1,36 +1,36 @@
 <template>
   <a-form-item label="字段标识">
-    <a-input v-model="config.config.id" allow-clear />
+    <a-input v-model="widget.id" allow-clear />
   </a-form-item>
   <a-form-item label="字段标签">
-    <a-input v-model="config.config.label" allow-clear />
+    <a-input v-model="widget.label" allow-clear />
   </a-form-item>
   <a-form-item label="分数上限">
-    <a-input-number v-model="config.config.count" />
+    <a-input-number v-model="widget.count" />
   </a-form-item>
   <a-form-item label="自定义颜色">
-    <a-input v-model="config.config.color" allow-clear />
+    <a-input v-model="widget.color" allow-clear />
   </a-form-item>
   <div class="boolean-config">
     <span class="label">允许半选</span>
-    <a-switch v-model="config.config.allowHalf" />
+    <a-switch v-model="widget.allowHalf" />
   </div>
   <div class="boolean-config mt-4">
     <span class="label">是否只读</span>
-    <a-switch v-model="config.config.readonly" />
+    <a-switch v-model="widget.readonly" />
   </div>
   <div class="boolean-config my-4">
     <span class="label">是否禁用</span>
-    <a-switch v-model="config.config.disabled" />
+    <a-switch v-model="widget.disabled" />
   </div>
   <a-form-item label="自定义校验规则">
     <a-textarea
-      v-model="config.config.rules"
+      v-model="widget.rules"
       :auto-size="{ minRows: 4, maxRows: 6 }"
     />
   </a-form-item>
   <a-form-item label="校验触发时机">
-    <a-select v-model="config.config.trigger" :allow-search="false" multiple>
+    <a-select v-model="widget.trigger" :allow-search="false" multiple>
       <a-option
         v-for="opt in inputEventNames"
         :key="opt"
@@ -53,8 +53,8 @@ const props = defineProps({
     required: true,
   },
 })
-const config = computed({
-  get: () => props.widgetConfig,
+const widget = computed({
+  get: () => props.widgetConfig.config,
   set: (val) => {
     emit('update:widgetConfig', val)
   },
